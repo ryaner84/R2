4bf489968b8fdaa712969c7aac5e0cac877b2f3d
Ryaner84 <yulyeern@hotmail.com>
2016-01-23 01:56:05 +0800

    assignment 2 question 1

---
+++ Week2Assignment/week2.R
@@ -0,0 +1,31 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  
+  ###STEP 1 Loading of DATA based on Directory Folder
+  filenamelist <-list.files(directory,full.names = TRUE)
+  
+
+  
+#   ###Using alapply and read.csv to apply all data into pollution data
+  pollutiondatatemp <- lapply(filenamelist, read.csv,header=T)
+  
+   ###Data is loaded in a list, i will combine them into 1 table
+  pollutiondata <-ldply(pollutiondatatemp, rbind)
+#     
+  ## BASED ON THE DATA FRAME ONLY SELECT THOSE ID I AM INTERESTED IN
+  
+  cALCULATIONDATA <-pollutiondata[pollutiondata$ID %in% id,]
+
+  
+
+  #calculation of mean based on the selected data only
+  
+
+  mean(cALCULATIONDATA[[pollutant]], na.rm=TRUE)
+  
+  
+  # mean(cALCULATIONDATA$pollutant,na.rm = TRUE)
+
+       
+}
+  
+  
